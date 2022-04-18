c8f3d43b42f175cef8ee3cacb40d4d9cc6b88c7e
EnzoHenrico <enzo.mascia1@gmail.com>
2022-04-18 15:01:17 -0300

    add: Arquivo checklist apra organização e declarados metodos de validação. Adicionado algoritimo para divisão dos dados da mensagem

--- java/Solution.java
+++ java/Solution.java
@@ -1,18 +1,76 @@
+import java.lang.reflect.Array;
 import java.util.*;
-
 public class Solution {
-  // Essa função recebe uma lista de mensagens, por exemplo:
-  //
-  // [
-  //   "72ff1d14-756a-4549-9185-e60e326baf1b,proposal,created,2019-11-11T14:28:01Z,80921e5f-4307-4623-9ddb-5bf826a31dd7,1141424.0,240",
-  //   "af745f6d-d5c0-41e9-b04f-ee524befa425,warranty,added,2019-11-11T14:28:01Z,80921e5f-4307-4623-9ddb-5bf826a31dd7,31c1dd83-8fb7-44ff-8cb7-947e604f6293,3245356.0,DF",
-  //   "450951ee-a38d-475c-ac21-f22b4566fb29,warranty,added,2019-11-11T14:28:01Z,80921e5f-4307-4623-9ddb-5bf826a31dd7,c8753500-1982-4003-8287-3b46c75d4803,3413113.45,DF",
-  //   "66882b68-baa4-47b1-9cc7-7db9c2d8f823,proponent,added,2019-11-11T14:28:01Z,80921e5f-4307-4623-9ddb-5bf826a31dd7,3f52890a-7e9a-4447-a19b-bb5008a09672,Ismael Streich Jr.,42,62615.64,true"
-  // ]
-  //
-  // Complete a função para retornar uma String com os IDs das propostas válidas no seguinte formato (separado por vírgula):
-  // "52f0b3f2-f838-4ce2-96ee-9876dd2c0cf6,51a41350-d105-4423-a9cf-5a24ac46ae84,50cedd7f-44fd-4651-a4ec-f55c742e3477"
+
+  public static void main(String[] args) {
+
+    List<String> messages = new ArrayList<String>();
+    List<String[]> dataList = new ArrayList<String[]>();
+    String[][] data = new String[4][10];
+
+    String messageData[] = {
+      
+    "72ff1d14-756a-4549-9185-e60e326baf1b,proposal,created,2019-11-11T14:28:01Z,80921e5f-4307-4623-9ddb-5bf826a31dd7,1141424.0,240",
+
+    "af745f6d-d5c0-41e9-b04f-ee524befa425,warranty,added,2019-11-11T14:28:01Z,80921e5f-4307-4623-9ddb-5bf826a31dd7,31c1dd83-8fb7-44ff-8cb7-947e604f6293,3245356.0,DF",
+
+    "450951ee-a38d-475c-ac21-f22b4566fb29,warranty,added,2019-11-11T14:28:01Z,80921e5f-4307-4623-9ddb-5bf826a31dd7,c8753500-1982-4003-8287-3b46c75d4803,3413113.45,DF",
+
+    "66882b68-baa4-47b1-9cc7-7db9c2d8f823,proponent,added,2019-11-11T14:28:01Z,80921e5f-4307-4623-9ddb-5bf826a31dd7,3f52890a-7e9a-4447-a19b-bb5008a09672,Ismael Streich Jr.,42,62615.64,true"
+    };
+
+    // Loop to insert recived data in a ArrayList
+    for (int i = 0; i < messageData.length; i++){
+      
+      messages.add(messageData[i]);
+    }
+
+    for (int i = 0; i < messages.size(); i++){
+
+      data[i] = messages.get(i).split(",");
+    
+      dataList.add(data[i]);
+      
+      System.out.println(data[i][0]);
+    }
+  
+    //processMessages(messages);
+  }
+
   public static String processMessages(List<String> messages) {
+
+  /** 
+    * ↓ All message sumary ↓ 
+    * event_id - (id da transação): [id][0]
+    * event_schema - (tipo de transação): [id][1]
+    * event_action - (tipo de atualização): [id][2]
+    * event_timestampv - (data do evento): [id][3]
+  */ 
+
+  List<String[]> dataList = new ArrayList<String[]>();
+  
+
+  for (int i = 0; i < messages.size(); i++){
+
+    String[] data = messages.get(i).split(",");
+
+    dataList.add(data);
+    
+    System.out.println(data);
+  }
+
+
     return "";
+  }
+
+
+  public static Boolean validateProposal(){
+    return true;
+  }
+  public static Boolean validateWarantty(){
+    return true;
+  }
+  public static Boolean validateProponent(){
+    return true;
   }
 }
---
+++ java/checklist.md
@@ -0,0 +1,59 @@
+# Guia de desenvolvimento
+
+Desafio em Java, código que consiga aprovar as propostas de financiamento baseado em uma lista de dados de usuário.
+
+## A fazer:
+
+- [x] Declarar funções para tratar tipos de mensagem diferentes
+- [x] Desestruturar as mensagens em dados separados
+- [ ] Armazenar os dados em variaveis
+- [ ] Desenvolver as funções declaradas
+- [ ] Desenvolver condicionais para aprovação das propostas
+- [ ] Estruturar dados para resposta
+- [ ] Passar nos teste xD
+
+## Regras do evento
+
+- O valor do empréstimo deve estar entre R$ 30.000,00 e R$ 3.000.000,00
+- O empréstimo deve ser pago em no mínimo 2 anos e no máximo 15 anos
+- Deve haver no mínimo 2 proponentes por proposta
+- Deve haver exatamente 1 proponente principal por proposta
+- Todos os proponentes devem ser maiores de 18 anos
+- Dever haver no mínimo 1 garantia de imóvel por proposta
+- A soma do valor das garantias deve ser maior ou igual ao dobro do valor do empréstimo
+- As garantias de imóvel dos estados PR, SC e RS não são aceitas
+- A renda do proponente principal deve ser pelo menos:
+  - 4 vezes o valor da parcela do empréstimo, se a idade dele for entre 18 e 24 anos
+  - 3 vezes o valor da parcela do empréstimo, se a idade dele for entre 24 e 50 anos
+  - 2 vezes o valor da parcela do empréstimo, se a idade dele for acima de 50 anos
+- Em caso de eventos repetidos, considere o primeiro evento
+  - Por exemplo, ao receber o evento ID 1 e novamente o mesmo evento, descarte o segundo evento
+- Em caso de eventos atrasados, considere sempre o evento mais novo
+  - Por exemplo, ao receber dois eventos de atualização com IDs diferentes, porém o último evento tem um timestamp mais antigo do que o primeiro, descarte o evento mais antigo
+
+## Estrutura dos dados
+
+- Lista recebida:
+
+[
+"72ff1d14-756a-4549-9185-e60e326baf1b,proposal,created,2019-11-11T14:28:01Z,80921e5f-4307-4623-9ddb-5bf826a31dd7,1141424.0,240",
+
+    "af745f6d-d5c0-41e9-b04f-ee524befa425,warranty,added,2019-11-11T14:28:01Z,80921e5f-4307-4623-9ddb-5bf826a31dd7,31c1dd83-8fb7-44ff-8cb7-947e604f6293,3245356.0,DF",
+
+    "450951ee-a38d-475c-ac21-f22b4566fb29,warranty,added,2019-11-11T14:28:01Z,80921e5f-4307-4623-9ddb-5bf826a31dd7,c8753500-1982-4003-8287-3b46c75d4803,3413113.45,DF",
+
+    "66882b68-baa4-47b1-9cc7-7db9c2d8f823,proponent,added,2019-11-11T14:28:01Z,80921e5f-4307-4623-9ddb-5bf826a31dd7,3f52890a-7e9a-4447-a19b-bb5008a09672,Ismael Streich Jr.,42,62615.64,true"
+
+]
+
+- Mensagem de cada elemento da lista:
+
+Mensagem do evento: "72ff1d14-756a-4549-9185-e60e326baf1b,proposal,created,2019-11-11T14:28:01Z,80921e5f-4307-4623-9ddb-5bf826a31dd7,1141424.0,240",
+
+event_id: 72ff1d14-756a-4549-9185-e60e326baf1b,
+event_schema: proposal
+event_action: created
+event_timestamp: created,2019-11-11T14:28:01Z
+proposal_id: 80921e5f-4307-4623-9ddb-5bf826a31dd7,
+proposal_loan_value: 1141424.0
+proposal_number_of_munthlyinstallments: 240
